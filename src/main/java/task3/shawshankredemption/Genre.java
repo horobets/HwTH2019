69541538af419e3b7e556b856d2a465053462b46
horobets <40602367+horobets@users.noreply.github.com>
2019-04-07 22:05:38 +0300

    Optimized comments

--- src/main/java/task3/shawshankredemption/Genre.java
+++ src/main/java/task3/shawshankredemption/Genre.java
@@ -3,7 +3,6 @@ package task3.shawshankredemption;
 import java.util.HashMap;
 import java.util.Map;
 
-//https://howtodoinjava.com/java/enum/java-enum-string-example/
 public enum Genre {
     HORROR("Horror"),
     BIOGRAPHY("Biography"),
@@ -21,7 +20,7 @@ public enum Genre {
         return description;
     }
 
-    // Reverse Lookup
+    // Reverse Lookup (ref: https://howtodoinjava.com/java/enum/java-enum-string-example/ )
     private static final Map<String, Genre> lookup = new HashMap<String, Genre>();
 
     //Populate the lookup table on loading time
@@ -36,35 +35,3 @@ public enum Genre {
         return lookup.get(description);
     }
 }
-
-
-/*
-Drama (1,555,713)
-Romance (479,606)
-Comedy (227,574)
-Short (213,565)
-Crime (156,360)
-Mystery (103,783)
-Thriller (103,090)
-Family (94,265)
-Action (89,220)
-Fantasy (60,669)
-Adventure (54,977)
-Sci-Fi (40,433)
-Animation (34,472)
-History (31,227)
-Horror (27,514)
-Music (22,592)
-War (22,127)
-Documentary (17,794)
-Biography (17,782)
-Musical (16,218)
-Sport (9,629)
-Reality-TV (9,167)
-Talk-Show (7,485)
-Western (6,563)
-News (2,062)
-Game-Show (981)
-Film-Noir (702)
-Adult (3)
- */
