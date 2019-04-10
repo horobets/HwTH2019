ee4c1a44a9a530005f4535bdccf2ff75e9936be7
horobets <40602367+horobets@users.noreply.github.com>
2019-04-10 21:07:46 +0300

    Added FB UserContent By

--- src/main/java/task5/facebooksearch/FbNewsFeedPage.java
+++ src/main/java/task5/facebooksearch/FbNewsFeedPage.java
@@ -9,6 +9,9 @@ public class FbNewsFeedPage extends BasePage {
     }
 
     private By fbUsername = By.cssSelector("#email");
+    //private By hyperFeedItemsBy = By.xpath("//div[contains(@id, 'hyperfeed_story')]");
+    private By hyperFeedItemsBy = By.cssSelector("div[id^='hyperfeed_story_id']");
+    private By hyperFeedItemsUserContentBy = By.cssSelector("div[id^='hyperfeed_story_id'] .userContent > div > p");
 
     public void goToPage() {
         driver.navigate().to("https://www.facebook.com/?sk=nf");
