4e378bf72074eb7644e412c467f6c1f543660962
horobets <40602367+horobets@users.noreply.github.com>
2019-05-01 21:13:31 +0300

    Sea Battle placeholder

---
+++ src/main/java/task8/seabattle/PlaySeaBattle.java
@@ -0,0 +1,12 @@
+package task8.seabattle;
+
+public class PlaySeaBattle {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Sea Battle %n%n");
+
+
+        System.out.printf("Thanks for playing! %n");
+    }
+}
