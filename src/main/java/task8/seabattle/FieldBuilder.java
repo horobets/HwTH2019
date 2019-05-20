54de1bebad249b8c1217dc9f513d68360492e51d
horobets <40602367+horobets@users.noreply.github.com>
2019-05-20 17:59:55 +0300

    game setup

--- src/main/java/task8/seabattle/FieldBuilder.java
+++ src/main/java/task8/seabattle/FieldBuilder.java
@@ -1,13 +1,16 @@
 package task8.seabattle;
 
+import task8.seabattle.battlefield.FieldPrinterMarker;
 import task8.seabattle.battlefield.SeaBattleField;
+import task8.seabattle.ships.ShipDirection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FieldBuilder {
 
-    private final List<Integer> availableShips;
+    private final List<Integer> remainingShipsSizes;
     private SeaBattleField field;
 
     public FieldBuilder() {
@@ -31,15 +34,82 @@ public class FieldBuilder {
         shipsLengths.add(1);
         shipsLengths.add(1);
 
-        this.availableShips = shipsLengths;
+        this.remainingShipsSizes = shipsLengths;
     }
 
-    public FieldBuilder(List<Integer> availableShips) {
-        this.availableShips = availableShips;
+    public FieldBuilder(List<Integer> remainingShipsSizes) {
+        this.remainingShipsSizes = remainingShipsSizes;
     }
 
     public SeaBattleField getField() {
         return field;
     }
 
+    public void printRemainingShips(){
+        for(int i = 0; i< remainingShipsSizes.size(); i++){
+
+            System.out.printf("%n%d. ", i+1);
+            printShipBySize(remainingShipsSizes.get(i), ShipDirection.LEFT);
+
+        }
+    }
+    public void printShipBySize(int shipSize, ShipDirection shipDirection){
+
+        System.out.printf("%n");
+
+        if(shipDirection == ShipDirection.UP || shipDirection == ShipDirection.DOWN )
+        for(int j = 0; j< shipSize; j++){
+            System.out.printf("%s", FieldPrinterMarker.SHIP);
+        }
+        else
+            for(int j = 0; j< shipSize; j++){
+                System.out.printf("%s\n", FieldPrinterMarker.SHIP);
+            }
+
+    }
+
+    public boolean startBuilding(boolean automatic){
+
+        Scanner scanner = new Scanner(System.in);
+        int userChoice;
+
+
+        while (true) {
+
+
+            System.out.printf("%n Select a ship (1 - %d): ", remainingShipsSizes.size());
+
+            int selectedRemainingShipSize = scanner.nextInt();
+
+
+            System.out.printf("%n Ship: ", remainingShipsSizes.size());
+            printShipBySize(remainingShipsSizes.get(selectedRemainingShipSize), ShipDirection.LEFT);
+
+            System.out.printf("%n Rotate the ship (1 - Up; 2 - Down; 3 - Left; 4 - Right): ", remainingShipsSizes.size());
+
+            int selectedDirectionIndex = scanner.nextInt()-1;
+            ShipDirection selectedShipDirection = ShipDirection.values()[selectedDirectionIndex-];
+
+
+            printShipBySize(remainingShipsSizes.get(selectedRemainingShipSize), selectedShipDirection);
+
+            System.out.printf("%n Place this ship to location: ");
+
+            printShipBySize(remainingShipsSizes.get(selectedRemainingShipSize), ShipDirection.LEFT);
+
+
+            switch (userChoice) {
+                case 1:
+                    playerRole = Role.X;
+                    break;
+                case 2:
+                    playerRole = Role.O;
+                    break;
+                default:
+                    System.out.printf("%nInvalid role selected: %d", userChoice);
+                    return;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/task8/seabattle/PlaySeaBattle.java
+++ src/main/java/task8/seabattle/PlaySeaBattle.java
@@ -4,11 +4,46 @@ import task8.seabattle.battlefield.*;
 import task8.seabattle.ships.ShipDirection;
 import task8.seabattle.ships.ShipType;
 
+import java.util.Scanner;
+
 public class PlaySeaBattle {
 
     public static void main(String[] args) {
 
         System.out.printf("Sea Battle %n%n");
+
+
+        Scanner scanner = new Scanner(System.in);
+        int userChoice;
+
+        GameMode gameMode = GameMode.SINGLEPLAYER;
+
+            System.out.printf("%nGame Mode (1 - Single Player; 2 - Play against a friend; 3 - Autoplay): ");
+
+            userChoice = scanner.nextInt();
+
+            switch (userChoice) {
+                case 1:
+                    gameMode = GameMode.SINGLEPLAYER;
+                    break;
+                case 2:
+                    gameMode = GameMode.MULTIPLAYER;
+                    break;
+                case 3:
+                    gameMode = GameMode.AUTOPLAY;
+                    break;
+                default:
+                    System.out.printf("%nInvalid mode selected: %d", userChoice);
+                    return;
+            }
+
+
+            System.out.printf("%n Player 1 Field:");
+
+        FieldBuilder fieldBuilder = new FieldBuilder();
+
+
+
 
 
         SeaBattleField battleField = new SeaBattleField();
