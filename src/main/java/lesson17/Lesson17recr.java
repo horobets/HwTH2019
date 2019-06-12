6fb6a6871e002dc78bcdfe528abd82197c2da3b9
horobets <40602367+horobets@users.noreply.github.com>
2019-06-12 22:48:12 +0300

    recr

--- src/main/java/lesson17/Lesson17recr.java
+++ src/main/java/lesson17/Lesson17recr.java
@@ -1,8 +1,22 @@
 package lesson17;
 
 public class Lesson17recr {
-    public String wordAppend(String str) {
-        return "";
+    public String parenBit(String str) {
+
+        if(str.length()<2){
+            return str;
+        }
+        else
+        {
+            if(str.charAt(str.length()-1)!=')')
+                return str.substring(0, str.length()-2);
+            else {
+                if (str.charAt(0) == '(')
+                    return str;
+                else
+                    return str.charAt(0) + parenBit(str.substring(1));
+            }
+        }
 
     }
 
